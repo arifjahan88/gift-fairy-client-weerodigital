ad2739b698d9d0a4c6f98ff925e2497edfce8f70
Arif Jahan <56018713+arifjahan88@users.noreply.github.com>
2023-04-08 03:34:56 +0600

    after all done

--- src/Components/Home/ContactPart/ContactPart.js
+++ src/Components/Home/ContactPart/ContactPart.js
@@ -5,21 +5,13 @@ const ContactPart = () => {
   return (
     <div className="bg-[#1371B9] py-14 lg:my-[100px] my-10">
       <div className="lg:flex justify-between max-w-[1334px] mx-auto">
-        <p
-          data-aos="fade-left"
-          data-aos-duration="1000"
-          className="lg:text-5xl font-semibold text-white text-center"
-        >
+        <p className="lg:text-5xl font-semibold text-white text-center">
           If you want to{" "}
           <span className="hover:text-white text-yellow-500 duration-500">learn more </span>
           about us, <br />
           get in touch with us.
         </p>
-        <div
-          data-aos="fade-right"
-          data-aos-duration="1000"
-          className="flex items-center justify-center"
-        >
+        <div className="flex items-center justify-center">
           <Link to="contact">
             <button className="lg:px-7 lg:py-3 px-3 py-1 bg-yellow-500 hover:bg-green-600 hover:text-white duration-500 rounded-xl lg:text-xl uppercase font-bold">
               Contact Us
--- src/Components/Home/Hero/Hero.js
+++ src/Components/Home/Hero/Hero.js
@@ -6,16 +6,16 @@ const Hero = () => {
   return (
     <div>
       <div>
-        <img className="-translate-y-24 -z-30 w-full opacity-80" src={image} alt="" />
+        <img className="lg:-translate-y-24 -z-30 w-full opacity-80" src={image} alt="" />
       </div>
       <div
-        data-aos="fade-right"
+        data-aos="fade-up"
         data-aos-duration="1500"
-        className="text-center lg:text-right absolute lg:top-64 lg:right-52"
+        className="text-center lg:text-right lg:absolute lg:top-64 lg:right-52 top-10"
       >
-        <h2 className="text-2xl">Introducing Most giftable Item on</h2>
-        <h1 className="lg:text-8xl font-extrabold">GIFT FAIRY</h1>
-        <p className="text-3xl">
+        <h2 className="lg:text-2xl text-xl">Introducing Most giftable Item on</h2>
+        <h1 className="lg:text-8xl text-2xl font-extrabold">GIFT FAIRY</h1>
+        <p className="lg:text-3xl text-sm">
           Shop the Perfect Gift for Every Occasion <br />
           Find Unique and Thoughtful Presents on Our Website
         </p>
